004c0d958fbaf98af6c97589ca892a15b3a4ee06
Deo Lins <deocleciolins@gmail.com>
2020-05-09 17:15:43 -0300

    Inicio de estudo de assets laravel

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Painel de Administração</title>
+    <link rel="stylesheet" href="{{ url(mix('admin/css/style.css')) }}">
+</head>
+<body>
+    <h1>Painel de Administração - Deo Lins</h1>
+
+</body>
+</html>
---
+++ resources/views/site/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Treinamento Assets</title>
+    <link rel="stylesheet" href="{{ url(mix('site/css/style.css'))}}">
+</head>
+<body>
+    <h1>Treinamento Assets - Deoclecio Lins</h1>
+
+    <script src="{{url(mix('site/js/script.js'))}}"></script>
+</body>
+</html>
